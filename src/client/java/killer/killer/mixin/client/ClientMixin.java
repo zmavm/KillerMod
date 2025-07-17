7786150044c4aa11ea41fd66dfd19e238da03e8d
BuildTools <unconfigured@null.spigotmc.org>
2025-07-17 19:29:03 +0800

    files

--- src/client/java/killer/killer/mixin/client/ClientMixin.java
+++ src/client/java/killer/killer/mixin/client/ClientMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class ExampleClientMixin {
+public class ClientMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of Minecraft.run()V
