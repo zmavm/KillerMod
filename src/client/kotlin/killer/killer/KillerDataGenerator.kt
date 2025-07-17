c248fa45a80d0bc9f1cf85180f9365e5b4213264
GAR9 <75528251+zmavm@users.noreply.github.com>
2025-07-17 17:40:11 +0800

    Add files via upload

---
+++ src/client/kotlin/killer/killer/KillerDataGenerator.kt
@@ -0,0 +1,9 @@
+package killer.killer
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object KillerDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
