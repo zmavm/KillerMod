e6889c54a17ef1bdd9a841c07fab7392953e953b
BuildTools <unconfigured@null.spigotmc.org>
2025-07-17 21:58:26 +0800

    com.mojang.brigadider updata

--- src/main/java/killer/killer/commands/killer.java
+++ src/main/java/killer/killer/commands/killer.java
@@ -1,7 +1,9 @@
 package killer.killer.commands;
 
 import net.fabricmc.api.ModInitializer;
+import com.mojang.brigadier.Command;
 
+@SuppressWarnings("unused")
 public class killer implements ModInitializer {
 
     @Override
