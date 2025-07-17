d8c76f142ac49f12b450d792fdd8889683feef8d
BuildTools <unconfigured@null.spigotmc.org>
2025-07-17 17:48:27 +0800

    files

---
+++ bin/client/killer/killer/KillerClient.kt
@@ -0,0 +1,9 @@
+package killer.killer
+
+import net.fabricmc.api.ClientModInitializer
+
+object KillerClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
