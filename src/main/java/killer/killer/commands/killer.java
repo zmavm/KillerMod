4b984486398cd0fc54ea4530b07ffd978394e935
BuildTools <unconfigured@null.spigotmc.org>
2025-07-17 22:32:43 +0800

    updadatafas

--- src/main/java/killer/killer/commands/killer.java
+++ src/main/java/killer/killer/commands/killer.java
@@ -1,17 +1,6 @@
 package killer.killer.commands;
 
 import net.fabricmc.api.ModInitializer;
-import com.mojang.brigadier.Command;
-// getString(ctx, "string")
-import static com.mojang.brigadier.arguments.StringArgumentType.getString;
-// word()
-import static com.mojang.brigadier.arguments.StringArgumentType.word;
- // literal("foo")
-import static net.minecraft.server.command.CommandManager.literal;
- // argument("bar", word())
-import static net.minecraft.server.command.CommandManager.argument;
-// Import everything in the CommandManager
-import static net.minecraft.server.command.CommandManager.*;
 
 @SuppressWarnings("unused")
 public class killer implements ModInitializer {
